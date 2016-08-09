14558d514275a4d34ae6fa61a578fb54e8bac17f
VagrantZXC <374884069@qq.com>
2016-08-09 09:49:39 +0800

    Initial Commit

---
+++ StarDeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarDeProject
+//
+//  Created by star on 16/8/9.
+//  Copyright © 2016年 star. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
